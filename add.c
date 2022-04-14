62f46f43e835c4d3537d0947f31a408220d0703c
Ayush Patnayak <ayush.patnaik07@gmail.com>
2022-04-14 19:36:01 +0530

    add code

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter the first var: ");
+    scanf("%d", &num1);
+    printf("Enter the second var: ");
+    scanf("%d", &num2);
+    int sum = num1 + num2;
+    printf("The sum of the numbers is %d", sum);
+
+}
