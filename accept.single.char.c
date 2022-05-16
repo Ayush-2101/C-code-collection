bc1b079c9649934db89489ee01030429ec6bc941
Ayush Patnayak <ayush.patnaik07@gmail.com>
2022-05-16 10:46:07 +0530

    hhijijij

---
+++ accept.single.char.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    ch=getchar();
+    printf("Accepted Character:%c",ch);
+}
---
+++ accepting.string.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char name[20];
+    printf("\n programming in c");
+    gets(programming in c);
+}
