ed2817ae999a39159a8d40c10a58b37d75f5ca0d
Ayush Patnayak <ayush.patnaik07@gmail.com>
2023-04-11 11:16:01 +0530

    namciaks

--- add.c
+++ add.c
@@ -1,11 +1,5 @@
-#include <stdio.h>
-int main(){
-    int num1, num2;
-    printf("Enter the first var: ");
-    scanf("%d", &num1);
-    printf("Enter the second var: ");
-    scanf("%d", &num2);
-    int sum = num1 + num2;
-    printf("The sum of the numbers is %d", sum);
-
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
 }
